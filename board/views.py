030446df39b8bffc91e73a57adba8d9cc1dd88a3
coitpark <minhobag18@gmail.com>
2024-05-24 14:51:39 +0900

    feat : 게시물 수정 및 pagination

--- board/views.py
+++ board/views.py
@@ -19,19 +19,27 @@ def board(request):
         if serializer.is_valid():
             serializer.save()
             print('게시물 저장')
-            return JsonResponse(BoardRepository().get_board(), safe=False)
+            return JsonResponse({'data' : '작성이 완료 되었습니다!'}, safe=False)
         else:
             print('게시물 저장중 에러')
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "GET":
-        return JsonResponse(BoardRepository().get_board(), safe=False)
+        if 'customId' in request.GET:
+            customId = request.GET['customId']
+            return JsonResponse(BoardRepository().find_board_for_id(customId), safe=False)
+        else:
+            page = request.GET['page']
+            return JsonResponse(BoardRepository().get_page_board(page), safe=False)
     elif request.method == "PUT":
         updated_data = BoardRepository().update_board(request.data)
         return Response(updated_data, status=status.HTTP_200_OK)
     elif request.method == "DELETE":
-        success = BoardRepository().delete_board(request.data.get('customId'))
+        print(request.GET['customId'])
+        success = BoardRepository().delete_board(request.GET['customId'])
         if success:
             return HttpResponse(status=204)
         else:
             return JsonResponse({'error': '해당 게시물을 찾을 수 없습니다.'}, status=404)
+
+
 
