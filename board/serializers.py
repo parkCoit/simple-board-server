688cb696f53e073eb2472c044fc191914e18f6c3
coitpark <minhobag18@gmail.com>
2024-07-05 16:55:49 +0900

    feat : 자동완성 설정

--- board/serializers.py
+++ board/serializers.py
@@ -11,8 +11,9 @@ class BoardSerializer(serializers.ModelSerializer):
         return Board.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        return Board.objects.filter(title=validated_data['old_title'], author=validated_data['author']).update(title=validated_data['new_title'],
-                                                                                 content=validated_data['new_content'])
+        return Board.objects.filter(title=validated_data['old_title'], author=validated_data['author']).update(
+            title=validated_data['new_title'],
+            content=validated_data['new_content'])
 
     def delete(self, instance, validated_data):
         pass
