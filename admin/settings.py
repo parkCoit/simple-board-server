9ad276b77858e6ff96f13357f1848f4688b7fd17
coitpark <minhobag18@gmail.com>
2025-05-23 17:36:49 +0900

    oracle

--- admin/settings.py
+++ admin/settings.py
@@ -29,9 +29,9 @@ with open(filename, "r") as f:
 SECRET_KEY = secrets["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['34.64.46.94', 'localhost', '127.0.0.1', 'coitpark.store']
+ALLOWED_HOSTS = secrets["ALLOWED_HOST"]
 
 # Application definition
 
@@ -171,6 +171,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
